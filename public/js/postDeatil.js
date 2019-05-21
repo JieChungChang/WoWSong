042971b9ca815f071b48458ec1667eabaa43e999
張捷中 <hahababee@gmail.com>
2019-05-21 20:42:26 +0800

    Modify post detail navigation list

--- public/js/postDeatil.js
+++ public/js/postDeatil.js
@@ -38,10 +38,11 @@ function isLogin(cookie) {
             console.log(res.memberData);
 
             navList1.setAttribute('href', '/post/createPost.html');
-            navList2.setAttribute('href', '/post/member?account='+res.memberData.account);
-
             navList1.innerText = '發表';
-            navList2.innerText = res.memberData.name;
+
+            addDropdownList(res.memberData.name, res.memberData.account, navList2 );
+            // navList2.setAttribute('href', '/post/member?account='+res.memberData.account);
+            // navList2.innerText = res.memberData.name;
 
             // 是否有追蹤
             const account = document.getElementById('memberAccount').innerText;
@@ -73,7 +74,68 @@ function isLogin(cookie) {
     });
 }
 // header 用 end
+/**
+ * 
+ * @param {*} name 
+ * @param {*} account 
+ * @param {*} node 
+ */
+function addDropdownList(name, account, node) {
+    const dropdownList = document.createElement('li');
+    dropdownList.setAttribute('class', 'nav-item dropdown');
 
+    const dropdownListName = document.createElement('a');
+    dropdownListName.setAttribute('class', 'nav-link dropdown-toggle');
+    dropdownListName.href = '#';
+    dropdownListName.id='navbarDropdownMenuLink';
+    dropdownListName.setAttribute('role', 'button');
+    dropdownListName.setAttribute('data-toggle', 'dropdown');
+    dropdownListName.setAttribute('aria-haspopup', 'true');
+    dropdownListName.setAttribute('aria-expanded', 'false');
+
+    const dropdownListNameText = document.createTextNode(name);
+    dropdownListName.appendChild(dropdownListNameText);
+
+    dropdownList.appendChild(dropdownListName);
+
+
+    const dropdownMenu = document.createElement('div');
+    dropdownMenu.setAttribute('class', 'dropdown-menu');
+    dropdownMenu.setAttribute('aria-labelledby', 'navbarDropdownMenuLink');
+
+
+    const dropdownMenuItemLink1 = document.createElement('a');
+    const dropdownMenuItemText1 = document.createTextNode('個人首頁');
+
+    dropdownMenuItemLink1.appendChild(dropdownMenuItemText1);
+
+    dropdownMenuItemLink1.href='/post/member?account='+account;
+    dropdownMenuItemLink1.setAttribute('class', 'dropdown-item');
+
+    dropdownMenu.appendChild(dropdownMenuItemLink1);
+
+
+    const dropdownMenuItemLink2 = document.createElement('a');
+    const dropdownMenuItemText2 = document.createTextNode('個人帳戶');
+
+    dropdownMenuItemLink2.appendChild(dropdownMenuItemText2);
+    dropdownMenuItemLink2.href='/member';
+    dropdownMenuItemLink2.setAttribute('class', 'dropdown-item');
+
+    dropdownMenu.appendChild(dropdownMenuItemLink2);
+
+    const dropdownMenuItemLink3 = document.createElement('a');
+    const dropdownMenuItemText3 = document.createTextNode('登出');
+    dropdownMenuItemLink3.setAttribute('class', 'dropdown-item');
+    dropdownMenuItemLink3.appendChild(dropdownMenuItemText3);
+    dropdownMenuItemLink3.setAttribute('onclick', 'logout()');
+
+    dropdownMenu.appendChild(dropdownMenuItemLink3);
+
+    dropdownList.appendChild(dropdownMenu);
+
+    node.parentNode.replaceChild(dropdownList, node);
+}
 /**
  * youtube view count
  */
